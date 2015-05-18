f1cd446b44450c8490eb84fc851933bd940999ec
Alain <alain.dutech@loria.fr>
2015-05-18 16:06:44 +0200

    ADD reservoir forward

--- src/reservoir.hpp
+++ src/reservoir.hpp
@@ -3,50 +3,69 @@
 #pragma once
 
 /** 
- * Création d'un réservoir random, seed depends on std::time()
+ * Création d'un réservoir random et uniform, seed depends on std::time().
+ *
+ * @param _input_scaling : input weights in [-input_scaling,input_sclaing]
  */
 
-#include <iostream>          // std::cout
-#include <sstream>           // std::stringstream
+#include <iostream>                     // std::cout
+#include <sstream>                      // std::stringstream
 
-#include <gsl/gsl_rng.h>     // gsl random generator
-#include <gsl/gsl_matrix.h>  // gsl Matrices
-#include <gsl/gsl_eigen.h>   // gsl EigenValues
-#include <gsl/gsl_complex_math.h>// gsl complex
-#include <ctime>             // std::time
+#include <gsl/gsl_rng.h>                // gsl random generator
+#include <gsl/gsl_matrix.h>             // gsl Matrices
+#include <gsl/gsl_eigen.h>              // gsl EigenValues
+#include <gsl/gsl_complex_math.h>       // gsl complex
+#include <gsl/gsl_blas.h>               // gsl matrix . vector multiplication
 
+#include <ctime>                        // std::time
+#include <vector>                       // std::vector
+#include <math.h>                       // tanh
+// ***************************************************************************
+// ***************************************************************** RESERVOIR
+// ***************************************************************************
 class Reservoir
 {
 public:
-  //typedef std::vector<double> Tinput;
+  typedef std::vector<double> Tinput;
   typedef unsigned int        Tinput_size;
   //typedef std::vector<double> Toutput;
   typedef unsigned int        Toutput_size;
   typedef gsl_matrix*         Tweights;
+  typedef gsl_vector*         Tstate;
 
-  // ****************************************************************** CREATION
-  /** Creation */
-  Reservoir( Tinput_size input_size, Toutput_size output_size ) :
-    _input_scaling( 1.0 ), _spectral_radius (1.0 ), _leaking_rate( 1.0 )
+// ****************************************************************** CREATION
+/** Creation */
+  Reservoir( Tinput_size input_size, Toutput_size output_size,
+	     double input_scaling = 1.0,
+	     double spectral_radius= 0.99,
+	     double leaking_rate = 0.1 ) :
+    _input_scaling(input_scaling), _spectral_radius(spectral_radius),
+    _leaking_rate(leaking_rate)
   {
     // Random generator with seed = time
     _rnd = gsl_rng_alloc( gsl_rng_taus );
     gsl_rng_set( _rnd, std::time( NULL ) );
-
-    // Matrix _output_size lines of _input_size columns
-    _w_in = gsl_matrix_alloc( output_size, input_size);
+    
+    // INPUT_WEIGHTS : Matrix _output_size lines of _input_size+1 columns
+    // in [- _input_scaling, _input_scaling]
+    _w_in = gsl_matrix_alloc( output_size, input_size+1);
     for( unsigned int i = 0; i < _w_in->size1; ++i) {
       for( unsigned int j = 0; j < _w_in->size2; ++j) {
-	gsl_matrix_set( _w_in, i, j, gsl_rng_uniform_pos(_rnd)-0.5 );
+	gsl_matrix_set( _w_in, i, j, (gsl_rng_uniform_pos(_rnd)-0.5) * _input_scaling / 0.5 );
       }
     }
-    // Matrix _output_size lines of _output_size columns
+    // RESERVOIR_WEIGHTS Matrix _output_size lines of _output_size columns
+    // in [-0.5, 0.5] before spectral radius
     _w_res = gsl_matrix_alloc( output_size, output_size);
     for( unsigned int i = 0; i < _w_res->size1; ++i) {
       for( unsigned int j = 0; j < _w_res->size2; ++j) {
 	gsl_matrix_set( _w_res, i, j, gsl_rng_uniform_pos(_rnd)-0.5 );
       }
     }
+    set_spectral_radius( _spectral_radius );
+
+    // RESERVOIR STATE : initialisé à 0
+    _x_res = gsl_vector_calloc( output_size );
   }
   /** Destruction */
   ~Reservoir()
@@ -54,8 +73,49 @@ public:
     gsl_rng_free( _rnd );
     gsl_matrix_free( _w_res );
     gsl_matrix_free( _w_in );
+    gsl_vector_free( _x_res );
   }
+  // ***************************************************************** OPERATION
+  void forward( const Tinput& in )
+  {
+    // Verifie bonne taille
+    if( (in.size()+1) != _w_in->size2 ) {
+      std::cerr << "Reservoir.forward() : Wrong input size !" << std::endl;
+      std::cerr << "                      in.size=" << in.size() << " != " << _w_in->size2 << std::endl;
+    }
+    // Into GSL : colomn matrix of in.size()+& row (for bias)
+    Tstate v_input = gsl_vector_alloc(in.size()+1 );
+    for( unsigned int i = 0; i< in.size(); ++i) {
+      gsl_vector_set( v_input, i, in[i]);
+    }
+    gsl_vector_set( v_input, in.size(), 1.0);
 
+    std::cout<< "IN= {" << str_vec(v_input) << std::endl;
+    
+    // Computation
+    Tstate v_tmp = gsl_vector_calloc( _x_res->size );
+    std::cout<< "TMP= {" << str_vec(v_tmp) << std::endl;
+    // _w_in * input + _w_res * _x_res
+    gsl_blas_dgemv(CblasNoTrans, 1.0, _w_res, _x_res, 0.0, v_tmp);  // tmp <- w_res * x_res
+    std::cout<< "TMP= {" << str_vec(v_tmp) << std::endl;
+    gsl_blas_dgemv(CblasNoTrans, 1.0, _w_in, v_input, 1.0, v_tmp); // tmp <- _w_in * v_in + tmp
+    std::cout<< "XN= {" << str_vec(v_tmp) << std::endl;
+    // tanh
+    for( unsigned int i = 0; i< v_tmp->size; ++i) {
+      gsl_vector_set( v_tmp, i, tanh( gsl_vector_get( v_tmp, i)));
+    }
+    std::cout<< "tanh(XN)= {" << str_vec(v_tmp) << std::endl;
+    // x = (1-alpha) x + alpha xtilde
+    gsl_vector_scale( v_tmp, _leaking_rate );
+    gsl_vector_scale( _x_res, 1.0 - _leaking_rate );
+    gsl_vector_add( _x_res, v_tmp);
+    std::cout<< "X= {" << str_vec(_x_res) << std::endl;
+
+    // Libère la mémoire
+    gsl_vector_free(v_input);
+    gsl_vector_free(v_tmp);
+    
+  }
   // ********************************************************************** INIT
   void set_spectral_radius( double radius )
   {
@@ -106,24 +166,37 @@ public:
   {
     std::stringstream dump;
     dump << "__WEIGHTS_IN__" << std::endl;
-    for( unsigned int i = 0; i < _w_in->size1; ++i) {
-      for( unsigned int j = 0; j < _w_in->size2; ++j) {
-	dump << gsl_matrix_get( _w_in, i, j ) << "; ";
-      }
-      dump << std::endl;
-    }
+    dump << str_mat( _w_in );
+
     dump << "__WEIGHTS_RESERVOIR__" << std::endl;
-    for( unsigned int i = 0; i < _w_res->size1; ++i) {
-      for( unsigned int j = 0; j < _w_res->size2; ++j) {
-	dump << gsl_matrix_get( _w_res, i, j ) << "; ";
-      }
-      dump << std::endl;
-    }
+    dump << str_mat( _w_res );
+
     return dump.str();
   };
+  /** display vector */
+  std::string str_vec(const Tstate v)
+  {
+    std::stringstream str;
+    for( unsigned int i = 0; i< v->size; ++i) {
+      str << gsl_vector_get(v, i) << "; ";
+    }
+    str << "}";
+    return str.str();
+  }
+  /** display matrix */
+  std::string str_mat(const Tweights m)
+  {
+    std::stringstream str;
+    for( unsigned int i = 0; i < m->size1; ++i) {
+      for( unsigned int j = 0; j < m->size2; ++j) {
+	str << gsl_matrix_get( m, i, j ) << "; ";
+      }
+      str << std::endl;
+    }
+    return str.str();
+  }
   
-  
-  
+  // **************************************************************** ATTRIBUTES
 private:
   //Tinput_size _input_size;
   //Toutput_size _output_size;
@@ -131,6 +204,8 @@ private:
   Tweights _w_in;
   /** Reservoir weights */
   Tweights _w_res;
+  /** Reservoir state */
+  Tstate _x_res;
   
   /** Parameters */
   double _input_scaling;
--- test/test-008-esn.cpp
+++ test/test-008-esn.cpp
@@ -13,14 +13,20 @@
 //******************************************************************************
 int main( int argc, char *argv[] )
 {
-  Reservoir res( 2, 3 );
+  Reservoir res( 2, 3, 0.2);
   std::cout << res.str_dump() << std::endl;
-  res.set_spectral_radius( 1.0 );
+  res.set_spectral_radius( 0.1 );
   std::cout << "** APRES CALCUL **" << "\n";
   std::cout << res.str_dump() << std::endl;
-  res.set_spectral_radius( 1.0 );
+  res.set_spectral_radius( 0.1 );
   std::cout << "** APRES 2eme CALCUL **" << "\n";
   std::cout << res.str_dump() << std::endl;
+
+  // try Input
+  Reservoir::Tinput in = {1.2, -2.0};
+  res.forward( in );
+  res.forward( in );
+  
   
   return 0;
 }
