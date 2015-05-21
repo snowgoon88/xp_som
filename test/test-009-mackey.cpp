f1a5756400844d5656d91dae1557773c3082d7b9
Alain <alain.dutech@loria.fr>
2015-05-21 10:20:00 +0200

    ADD try GAML::make_output_stream

--- test/test-009-mackey.cpp
+++ test/test-009-mackey.cpp
@@ -10,6 +10,19 @@
 
 #include <mackeyglass.hpp>
 
+#include <gaml.hpp>       // GAML Library
+
+/** Parser for Mackeyglass */
+struct SimpleParser {
+  typedef double value_type;
+  void read(std::ostream& os, value_type& x) const {
+    // Not needed
+  }
+  void write(std::ostream& os, const value_type& x) const {
+    os << x;
+  }
+};
+
 //******************************************************************************
 int main( int argc, char *argv[] )
 {
@@ -20,7 +33,16 @@ int main( int argc, char *argv[] )
     std::cout << x << "; ";
   }
   std::cout << "}" << std::endl;
-  
+
+  // Try to use GAML output_streams => pas très utile ici au vu des données.
+  // [Q] et pour ajouter des commentaires en haut du fichier ??
+  SimpleParser my_parser;
+  auto output_stream = gaml::make_output_data_stream(std::cout, my_parser);
+  auto out1 = gaml::make_output_iterator(output_stream);
+  std::cout << std::endl << "GAML built-in output parser" << std::endl
+	    << std::endl;
+  std::copy(data.begin(), data.end(), out1);
+
   return 0;
 }
 
