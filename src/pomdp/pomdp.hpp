a80cab42b27e0f216f86d60069475b25ec06916b
Alain <alain.dutech@loria.fr>
2016-01-07 14:02:43 +0100

    ADD pompd : read/load/simulate

--- src/pomdp/pomdp.hpp
+++ src/pomdp/pomdp.hpp
@@ -5,7 +5,7 @@
 
 /** 
  * Outils pour utiliser des POMDP
- * - Charger/Lire
+ * - Charger/Lire avec JSON
  * - Simuler 
  */
 
@@ -15,6 +15,11 @@
 
 #include <gsl/gsl_rng.h>             // gsl random generator
 #include <ctime>                     // std::time
+
+#include "rapidjson/prettywriter.h" // rapidjson
+#include "rapidjson/document.h"     // rapidjson's DOM-style API
+// #include <json_wrapper.hpp>         // JSON::OStreamWrapper et IStreamWrapper
+namespace rj = rapidjson;
 
 // ********************************************************************* Model
 namespace Model
@@ -28,6 +33,21 @@ struct Node
   std::string _label;
   std::string str_dump() const { 
     return std::to_string(_id)+std::string(":")+_label;
+  }
+  rj::Value serialize( rj::Document& doc )
+  {
+    // rj::Object qui contient les données
+    rj::Value rj_node;
+    rj_node.SetObject();
+    rj_node.AddMember( "id", rj::Value(_id), doc.GetAllocator() );
+    rj_node.AddMember( "label", rj::StringRef(_label.c_str()), doc.GetAllocator() );
+    
+    return rj_node;
+  }
+  void unserialize( const rj::Value& obj )
+  {
+    _id = obj["id"].GetUint();
+    _label = std::string( obj["label"].GetString() );
   }
 };
 // ***************************************************************************
@@ -53,7 +73,7 @@ public:
     }
     return _proba.size()-1;
   };
-  // ******************************************************** Transistion::str
+  // ********************************************************* Transition::str
   std::string str_dump() const
   {
     std::stringstream dump;
@@ -64,6 +84,30 @@ public:
     dump << "]";
     return dump.str();
   };
+  // *************************************************** Transition::serialize
+  rj::Value serialize( rj::Document& doc )
+  {
+    // rj::Array qui contient les données
+    rj::Value rj_trans;
+    rj_trans.SetArray();
+    for( auto& prob: _proba) {
+      rj_trans.PushBack( prob, doc.GetAllocator());
+    }
+    
+    return rj_trans;
+  }
+  // ************************************************* Transition::unserialize
+  void unserialize( const rapidjson::Value& obj )
+  {
+    _proba.clear();
+    const rj::Value& ar = obj;
+    assert( ar.IsArray() );
+    rapidjson::SizeType idx = 0;
+    for( idx=0; idx < ar.Size(); ++idx ) {
+      assert( ar[idx].IsNumber() );
+      _proba.push_back( ar[idx].GetDouble() );
+    }
+  }
 };
 // ***************************************************************************
 // ********************************************************************* POMDP
@@ -89,6 +133,23 @@ public:
     gsl_rng_set( _rnd, std::time( NULL ) );
     // obs
     simul_obs();
+  };
+  POMDP( const rj::Value& obj ) :
+    _states(), _obs(), _actions(),
+    _trans(), _percep(), _reward(),
+    _rnd(nullptr),
+    _cur_state(), _cur_obs()
+  {
+    // decode d'après obj
+    unserialize( obj );
+    
+    // Générateur Aléatoire
+    _rnd = gsl_rng_alloc( gsl_rng_taus );
+    gsl_rng_set( _rnd, std::time( NULL ) );
+
+    // _cur_state(states[0])
+    // obs
+    // simul_obs();
   };
   // ************************************************************** POMDP::str
   /** dump state */
@@ -121,7 +182,7 @@ public:
     dump << "TRANSITIONS : " << std::endl;
     for( auto& s: _states) {
       for( auto& a: _actions) {
-	dump << "  T(" << s._label << "; " << a._label << ") = " << _trans[s._id][a._id].str_dump() << std::endl; 
+    	dump << "  T(" << s._label << "; " << a._label << ") = " << _trans[s._id][a._id].str_dump() << std::endl; 
       }
     }
     dump << "PERCEPTION : " << std::endl;
@@ -134,6 +195,126 @@ public:
     }
     
     return dump.str();
+  };
+  // ******************************************************** POMDP::serialize
+  rj::Value serialize( rj::Document& doc )
+  {
+    // rj::Object qui contient les données
+    rj::Value obj;
+    obj.SetObject();
+
+    // Ajoute les états sous forme d'array
+    rj::Value ar_state;
+    ar_state.SetArray();
+    for( auto& s: _states) {
+      ar_state.PushBack( s.serialize(doc), doc.GetAllocator() );
+    }
+    obj.AddMember( "states", ar_state, doc.GetAllocator() );
+    // Ajoute les actions sous forme d'array
+    rj::Value ar_act;
+    ar_act.SetArray();
+    for( auto& a: _actions) {
+      ar_act.PushBack( a.serialize(doc), doc.GetAllocator() );
+    }
+    obj.AddMember( "actions", ar_act, doc.GetAllocator() );
+    // Ajoute les observations sous forme d'array
+    rj::Value ar_obs;
+    ar_obs.SetArray();
+    for( auto& o: _obs) {
+      ar_obs.PushBack( o.serialize(doc), doc.GetAllocator() );
+    }
+    obj.AddMember( "obs", ar_obs, doc.GetAllocator() );
+
+    // Les transitions
+    rj::Value ar_trans;
+    ar_trans.SetArray();
+    for( auto& s: _states) {
+      for( auto& a: _actions) {
+	ar_trans.PushBack( _trans[s._id][a._id].serialize( doc ), doc.GetAllocator());
+      }
+    }
+    obj.AddMember( "trans", ar_trans, doc.GetAllocator());
+    // Les fonction de perception
+    rj::Value ar_per;
+    ar_per.SetArray();
+    for( auto& s: _states) {
+      ar_per.PushBack( _percep[s._id].serialize( doc ), doc.GetAllocator());
+    }
+    obj.AddMember( "perc", ar_per, doc.GetAllocator());
+
+    // Les Reward
+    rj::Value ar_rew;
+    ar_rew.SetArray();
+    for( auto& s: _states) {
+      ar_rew.PushBack( _reward[s._id], doc.GetAllocator() );
+    }
+    obj.AddMember( "reward", ar_rew, doc.GetAllocator() );
+    
+    return obj;
+  }
+  void unserialize( const rapidjson::Value& obj )
+  {
+    // Array of states
+    const rj::Value& ar_states = obj["states"];
+    assert( ar_states.IsArray() );
+    rj::SizeType idx = 0;
+    for( idx=0; idx < ar_states.Size(); ++idx ) {
+      Node n;
+      n.unserialize( ar_states[idx] );
+      _states.push_back( n );
+    }
+    // Array of actions
+    const rj::Value& ar_actions = obj["actions"];
+    assert( ar_actions.IsArray() );
+    for( idx=0; idx < ar_actions.Size(); ++idx ) {
+      Node n;
+      n.unserialize( ar_actions[idx] );
+      _actions.push_back( n );
+    }
+    // Array of obs
+    const rj::Value& ar_obs = obj["obs"];
+    assert( ar_obs.IsArray() );
+    for( idx=0; idx < ar_obs.Size(); ++idx ) {
+      Node n;
+      n.unserialize( ar_obs[idx] );
+      _obs.push_back( n );
+    }
+
+    // Transitions
+    const rj::Value& ar_trans = obj["trans"];
+    assert( ar_trans.IsArray() );
+    idx = 0;
+    for( unsigned int is = 0; is < _states.size(); ++is) {
+      std::vector<Transition> v_tr_a;
+      for( unsigned int ia = 0; ia < _actions.size(); ++ia) {
+	Transition tr;
+	tr.unserialize( ar_trans[idx] );
+	v_tr_a.push_back( tr );
+	idx++;
+      }
+      _trans.push_back( v_tr_a );
+    }
+
+    // Perceptions
+    const rj::Value& ar_perc = obj["perc"];
+    assert( ar_perc.IsArray() );
+    idx = 0;
+    for( unsigned int is = 0; is < _states.size(); ++is) {
+      Transition tr;
+      tr.unserialize( ar_perc[idx] );
+      _percep.push_back( tr );
+      idx++;
+    }
+
+    // Reward
+    const rj::Value& ar_rew = obj["reward"];
+    assert( ar_rew.IsArray() );
+    idx = 0;
+    for( unsigned int is = 0; is < _states.size(); ++is) {
+      assert( ar_rew[idx].IsNumber() );
+      _reward.push_back( ar_rew[idx].GetDouble() );
+      idx++;
+    }
   };
   // ************************************************************ POMDP::simul
   const Node& simul_trans(const Node& act)
--- test/test-014-pomdp.cpp
+++ test/test-014-pomdp.cpp
@@ -4,14 +4,25 @@
  * Teste Model::POMDP.
  *  - Node : creation, affichage
  *  - Transition : creation, affichage, get_next
- *  - POMDP : creation, affichage, simulation
+ *  - POMDP : creation, affichage, simulation, read/write
  */
 
 #include <iostream>                  // std::cout
+#include <fstream>                 // std::ofstream
+// #include <string>                  // std::string
+#include "rapidjson/document.h"         // rapidjson's DOM-style API
+#include <json_wrapper.hpp>          // JSON::OStreamWrapper, JSON::IStreamWrapper
 
 #include <pomdp.hpp>
 #include <gsl/gsl_rng.h>             // gsl random generator
 #include <ctime>                     // std::time
+
+#include <utils.hpp>                  // various str_xxx
+using namespace utils::rj;
+
+// ***************************************************************************
+#define FILENAME "pomdp_save.json"
+
 //******************************************************************************
 int main( int argc, char *argv[] )
 {
@@ -38,12 +49,43 @@ int main( int argc, char *argv[] )
   std::cout << "POMDP *******" << std::endl << pomdp.str_dump() << std::endl;
   std::cout << "CURRENT : " << pomdp.str_state() << std::endl;
 
+
+  // Serialisation dans FILENAME
+  std::cout << "SERIALIZE " << std::endl;
+  rapidjson::Document doc;
+  doc.SetObject();
+  doc.AddMember( "pomdp", pomdp.serialize(doc), doc.GetAllocator());
+
+  // Write to file
+  std::cout << "   => write to file" << std::endl;
+  std::ofstream ofile( FILENAME );
+  ofile << str_obj(doc) << std::endl;
+  ofile.close();
+
+  // Read from file
+  std::cout << "UNSERIALZE" << std::endl;
+  std::ifstream ifile( FILENAME );
+  // Wrapper pour lire document
+  JSON::IStreamWrapper instream(ifile);
+  // Parse into a document
+  rj::Document read_doc;
+  read_doc.ParseStream( instream );
+  ifile.close();
+
+  Model::POMDP read_pomdp( read_doc["pomdp"] );
+  std::cout << "READ POMDP *******" << std::endl << read_pomdp.str_dump() << std::endl;
+
   // Essai d'action
+  std::cout << "SIMULATION" << std::endl;
   const std::vector<Model::Node>& list_action = pomdp.actions();
-  // Transation
+
+  std::cout << "CURRENT : " << pomdp.str_state() << " r= " << pomdp.cur_reward() << std::endl;
+  
+  // Transition
   for( unsigned int i = 0; i < 5; ++i) {
     // Choix d'une action aléatoire
     const Model::Node& act = list_action[gsl_rng_uniform_int( rnd, list_action.size() )];
+    std::cout << "   +ACT : " << act.str_dump() << std::endl;
     pomdp.simul_trans( act );
     std::cout << "CURRENT : " << pomdp.str_state() << " r= " << pomdp.cur_reward() << std::endl;
   }
