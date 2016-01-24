78edb923c55af941675f92aa1ad75b625b9191a4
Alain Dutech <alain.dutech@loria.fr>
2016-01-24 17:22:39 +0100

    ADD noise data : create,read,write

---
+++ src/noise.hpp
@@ -0,0 +1,99 @@
+/* -*- coding: utf-8 -*- */
+
+#ifndef NOISE_HPP
+#define NOISE_HPP
+
+/** 
+ * Generate a multi-dimension noise.
+ * Can save into json.
+ * can Read/Write into file.
+ */
+
+#include <iostream>                   // std::cout, std::istream
+#include <sstream>                   // std::istringstream
+#include <ctime>                      // std::time
+#include <vector>                     // std::vector
+#include <gsl/gsl_rng.h>              // gsl random generator
+#include <gsl/gsl_randist.h>          // gsl random distribution
+
+class WNoise
+{
+public:
+  // ************************************************************ WNoise::Data
+  typedef std::vector<std::vector<double>> Data;
+  // ******************************************************** WNoise::creation
+  WNoise( const unsigned int nb_pt,
+	  const double level = 1.0,
+	  const unsigned int dim = 1,
+	  unsigned long int seed = std::time(NULL) ) :
+    _nb_pt(nb_pt), _level(level), _dim(dim),
+    _seed(seed)
+  {
+  };
+  // ********************************************************** WNoise::create
+  static Data create_sequence( const unsigned int nb_pt,
+			       const double level = 1.0,
+			       const unsigned int dim = 1,
+			       const unsigned long int seed = std::time(NULL) )
+  {
+    // Random Engine with seed 
+    const gsl_rng* rnd = gsl_rng_alloc( gsl_rng_taus );
+    gsl_rng_set( rnd, seed );
+
+    Data v_noise;
+    for( unsigned int i = 0; i < nb_pt; ++i) {
+      std::vector<double> element;
+      for( unsigned int j = 0; j < dim; ++j) {
+	element.push_back( (gsl_rng_uniform_pos(rnd)-0.5) * level );
+      }
+      v_noise.push_back( element );
+    }
+    return v_noise;
+  }
+  const Data& create_sequence()
+  {
+    _seq = create_sequence( _nb_pt, _level, _dim, _seed );
+    return _seq;
+  }
+  // ********************************************************** WNoise::parser
+  /** read/write for Mackeyglass */
+  static void read(std::istream& is, Data& data)
+  {
+    double x;
+    data.clear();
+    std::string line;
+    while (std::getline(is, line)) {
+      std::vector<double> vec;
+      std::istringstream iss(line);
+      while( !iss.eof()) {
+	iss >> x;
+	vec.push_back( x );
+      }
+      data.push_back( vec );
+    }
+  }
+  static void write(std::ostream& os, const Data& data)
+  {
+    for( auto& vec: data) {
+      for( auto& val: vec) {
+	os << val << "\t";
+      }
+      os << std::endl;
+    }
+  }
+  // ******************************************************* WNoise::attributs
+  const Data& data() const { return _seq; };
+private:
+  /** nb_pt generated */
+  unsigned int _nb_pt;
+  /** noise level */
+  double _level;
+  /** dimension */
+  unsigned int _dim;
+  /** seed */
+  unsigned long int _seed;
+  /** Sequence generated */
+  Data _seq;
+};
+
+#endif // NOISE_HPP
---
+++ test/test-015-noise.cpp
@@ -0,0 +1,71 @@
+/* -*- coding: utf-8 -*- */
+
+/** 
+ * test-015-noise.cpp
+ *
+ * Generate, print, write/save Noise sequence.
+ */
+
+#include <noise.hpp>
+
+#include <iostream>       // std::cout
+#include <fstream>        // std::ifile
+
+// ******************************************************************** Global
+#define NOISE_FILE "wnoise.data"
+
+//******************************************************************************
+int main( int argc, char *argv[] )
+{
+  // Deux façons de générer des séquences
+  // UN : un passant par un créateur, ce qui permet ensuite de serialiser
+  //      les paramètres.
+  WNoise wnoise( 20, 0.2, 3 );
+  wnoise.create_sequence();
+
+  // // Les param sous forme de JSON
+  // rapidjson::Document doc;
+  // rapidjson::Value obj = wnoise.serialize( doc );
+  // std::cout << "** JSON **" << std::endl;
+  // std::cout << str_obj( obj ) << std::endl;
+
+  // DEUX : en passant par la fonction statique qui crée une séquence.
+  WNoise::Data wnoise_data = WNoise::create_sequence( 20, 0.4, 2 );
+  
+  std::cout << "** WNOISE" << std::endl;
+  for( auto& vec: wnoise.data()) {
+    for( auto& var: vec) {
+      std::cout << var << "; ";
+    }
+    std::cout << std::endl;
+  }
+  
+  std::cout << "** WNOISE_DATA" << std::endl;
+  for( auto& vec: wnoise_data) {
+    for( auto& var: vec) {
+      std::cout << var << "; ";
+    }
+    std::cout << std::endl;
+  }
+
+  // Essai de sérialisation
+  std::ofstream ofile(NOISE_FILE);
+  WNoise::write( ofile, wnoise.data());
+  ofile.close();
+
+  // Et maintenant on relis
+  WNoise::Data data_read;
+  std::ifstream ifile(NOISE_FILE);
+  WNoise::read( ifile, data_read);
+  ifile.close();
+
+  // Compare les deux
+  std::cout << std::endl << "COMPARAISON data =?= read " << std::endl;
+  for( unsigned int i = 0; i < wnoise.data().size(); ++i) {
+    for( unsigned int j = 0; j < wnoise.data()[i].size(); ++j) {
+      std::cout << wnoise.data()[i][j] << "  =?= " << data_read[i][j] << std::endl;
+    }
+  }
+
+  return 0;
+}
