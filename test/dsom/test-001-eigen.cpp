9f3b5b23919dee91c3d630b73caddb9871f8dd19
Alain <alain.dutech@loria.fr>
2016-04-08 14:37:45 +0200

    ADD DSOM::Neurone, write to JSON

--- test/dsom/test-001-eigen.cpp
+++ test/dsom/test-001-eigen.cpp
@@ -8,6 +8,7 @@
 #include <iostream>       // std::cout
 #include <Eigen/Dense>
 
+/** Create and manipulate Vectors */
 void tt_vector()
 {
   Eigen::Vector3f v1(1.0, 0.5, 0.8);
@@ -18,10 +19,30 @@ void tt_vector()
   std::cout << "SUM POW v1-v2 = " << (v1-v2).cwiseProduct(v1-v2).sum() << "\n";
   std::cout << "SQRT SUM POW v1-v2 = " << sqrt((v1-v2).cwiseProduct(v1-v2).sum()) << "\n";
 }
+/** Create Random Matrices */
+void tt_matrix()
+{
+  for(int i = 0; i < 5; i++) {   
+    std::srand((unsigned int) time(0));
+    // Random matrix between -1 and 1
+    Eigen::VectorXd V = Eigen::VectorXd::Random(3);
+    std::cout << "vec=\n" << V << std::endl;
+    const Eigen::VectorXd& VP = Eigen::VectorXd::Random(3);
+    std::cout << "vec=\n" << VP << std::endl;
+    Eigen::MatrixXf A = Eigen::MatrixXf::Random(3, 3);
+    std::cout << "mat=\n" << A << std::endl;
+    // Scale between 10 and 20
+    A = (A.array() - -1.f) / (1.f - -1.f) * (20.f - 10.f) + 10.f;
+    std::cout << "[10,20]=\n" << A << std::endl;
+  }
+}
 
 // ***************************************************************************
 int main(int argc, char *argv[])
 {
+  std::cout << "__VECTOR" << std::endl;
   tt_vector();
+  std::cout << "__MATRIX" << std::endl;
+  tt_matrix();
   return 0;
 }
--- test/dsom/test-002-neuron.cpp
+++ test/dsom/test-002-neuron.cpp
@@ -5,8 +5,16 @@
  *
  * - create DSOM::Neuron
  */
-#include <iostream>       // std::cout
+#include <iostream>                     // std::cout
+#include <fstream>                      // std::ofstream
 #include <dsom/neuron.hpp>
+
+#include "rapidjson/document.h"         // rapidjson's DOM-style API
+#include <utils.hpp>                    // various str_xxx
+using namespace utils::rj;
+
+// ******************************************************************** Global
+#define N_FILE "neuron.data"
 
 /**
  * Create DSOM::Neuron
@@ -14,20 +22,54 @@
 void tt_create()
 {
   Model::DSOM::Neuron n(0, 3);
-  std::cout << n.dumpToString() << "\n";
+  std::cout << n.str_dump() << "\n";
 
   n.add_link( 1 );
   n.add_link( 5 );
-  std::cout << n.dumpToString() << "\n";
+  std::cout << n.str_dump() << "\n";
 
   n.add_neighbor( 1, 1.0 );
   n.add_neighbor( 2, 2.1 );
-  std::cout << n.dumpToString() << "\n";
+  std::cout << n.str_dump() << "\n";
 }
+/**
+ * Read/Write DSOM::Neuron
+ */
+void tt_neur_wr()
+{
+  Model::DSOM::Neuron n(0, 3);
+  n.add_link( 1 );
+  n.add_link( 5 );
+  std::cout << "*****\n" <<n.str_dump() << "\n";  
+
+  //*** JSON ***
+  rapidjson::Document doc;
+  rapidjson::Value obj = n.serialize( doc );
+  std::cout << str_obj( obj ) << std::endl;
+  // Write un a file
+  std::ofstream ofile(N_FILE);
+  ofile << str_obj( obj ) << std::endl;
+  ofile.close();
+  
+  //*** PERSISTENCE ***
+  // std::ofstream outfile( "neurone.sav" );
+  // n.write( outfile );
+  // outfile.close();
+
+  // Persistence save("neurone.sav" );
+
+  // DSOM::Neuron nr( save );
+  // std::cout << "*****\n" <<nr.dumpToString() << "\n";
+  
+}
+
 
 // ***************************************************************************
 int main(int argc, char *argv[])
 {
+  std::cout << "__CREATE Neurone" << std::endl;
   tt_create();
+  std::cout << "__READ/WRITE neurone" << std::endl;
+  tt_neur_wr();
   return 0;
 }
