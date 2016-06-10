a92d569725d629a14d0bab140af347930343bca1
Alain <alain.dutech@loria.fr>
2016-06-10 11:58:34 +0200

    ADD create/save/load Trajectory::HMM

---
+++ src/supelec/hmm_trajectory.hpp
@@ -0,0 +1,61 @@
+/* -*- coding: utf-8 -*- */
+
+#ifndef HMM_TRAJECTORY_HPP_HPP
+#define HMM_TRAJECTORY_HPP_HPP
+
+/** 
+ * Trajectory::HMM is a sequence of Trajectory::HMM:Item(s,o)
+ */
+
+// **************************************************************** Trajectory
+namespace Trajectory
+{
+// ***************************************************************************
+// *********************************************************************** HMM
+// ***************************************************************************
+class HMM
+{
+public:
+  // *************************************************************** HMM::Item
+  typedef struct {
+    int    id_s;
+    double id_o;
+  } Item;
+  // *************************************************************** HMM::Data
+  typedef std::vector<Item> Data;
+  // *************************************************************** HMM::save
+  static void save( std::ostream& os, const Data& data )
+  {
+    // header
+    os << "## \"length\": " << data.size() << std::endl;
+
+    for( const auto& item: data) {
+      os << item.id_s << "\t" << item.id_o << std::endl;
+    }
+  };
+  // ************************************************************* HMM::parser
+  static void read( std::istream& is, Data& data )
+  {
+    data.clear();
+    Item item;
+
+    // Lit en omettant les lignes commençant par '#'
+    std::string line;
+    while (!is.eof()) {
+      std::getline( is, line );
+      if( line.front() != '#' ) {
+	std::stringstream iss( line );
+	iss >> item.id_s >> item.id_o;
+	
+	// en testant !eof, on évite de lire le dernier endl comme un float...
+	if( !is.eof()) {
+	  data.push_back( item );
+	}
+      }
+    }
+  };
+};
+// *********************************************************************** HMM
+}; // namespace Trajectory
+
+#endif // HMM_TRAJECTORY_HPP_HPP
--- xp/xp-003-hmm.cpp
+++ xp/xp-003-hmm.cpp
@@ -4,9 +4,11 @@
  * WP with HMM (cmp with BICA's Paper de Jeremy and Herve
  *
  * - generate/save/load HMM : expr
- * - TODO generate Trajectories of (S,O). inspi from History ??
+ * - generate Trajectories of (S,O). inspi from Trajectory
  * - TODO save Trajectories
  * - TODO Load Trajectories
+ *
+ * TODO : make function that don't use globals...
  */
 
 #include <iostream>                // std::cout
@@ -19,15 +21,19 @@
 
 #include <hmm-json.hpp>
 #include <input.hpp>
+#include <hmm_trajectory.hpp>
 
 #include <utils.hpp>                  // various str_xxx
 using namespace utils::rj;
 // ******************************************************************** Global
-
+// HMM
 std::string _expr = "ABCD";
 bica::hmm::T _t;
 bica::hmm::O _o;
 unsigned int _nb_states;
+
+// Trajectory
+Trajectory::HMM::Data _learn_data;
 
 // **************************************************************** create_hmm
 void create_hmm( const std::string& expr = "ABCD" )
@@ -76,6 +82,39 @@ void print_hmm(const std::string& name, bica::sampler::HMM& h, int nb)
   std::cout << std::endl;
   std::cout << std::endl;
 }
+// *************************************************************** create_traj
+void create_traj( const unsigned int length )
+{
+  // Sampler pour le HMM
+  bica::sampler::HMM hmm(_t,_o);
+
+  _learn_data.clear();
+  for( unsigned int i = 0; i < length; ++i) {
+    _learn_data.push_back( Trajectory::HMM::Item{hmm.input_id(), hmm.input()} );
+    hmm.shift();
+  }
+};
+// ***************************************************************** save_traj
+void save_traj( const std::string& filename,
+		const Trajectory::HMM::Data& data,
+		const std::string hmm_expr)
+{
+  auto ofile = std::ofstream( filename );
+
+  // header
+  ofile << "## \"hmm_expr\": \"" << hmm_expr << "\"," << std::endl;
+
+  Trajectory::HMM::save( ofile, data );
+  
+  ofile.close();
+};
+// ***************************************************************** load_traj
+ void load_traj( const std::string& filename )
+{
+  auto pfile = std::ifstream( filename );
+  Trajectory::HMM::read( pfile, _learn_data );
+  pfile.close();
+};
 // ***************************************************************************
 // ********************************************************************** main
 // ***************************************************************************
@@ -85,22 +124,32 @@ int main(int argc, char *argv[])
   create_hmm( "ABCDCB" );
   bica::sampler::HMM hmm1(_t,_o);
   std::cout << "__" << _expr << "__ with " << _nb_states << " states" << std::endl;  
-  print_hmm("h1",hmm1,30);
-  // Save to JSON
-  save_hmm( "tmp_hmm.json", _expr );
-  // Reload from JSON
-  // first create another one
-  create_hmm( "AAAF" );
-  bica::sampler::HMM hmm2(_t,_o);
-  std::cout << "__" << _expr << "__ with " << _nb_states << " states" << std::endl;  
-  print_hmm("h2",hmm2,30);
-  // then load
-  load_hmm( "tmp_hmm.json" );
-  bica::sampler::HMM hmm3(_t,_o);
-  std::cout << "__" << _expr << "__ with " << _nb_states << " states" << std::endl;  
-  print_hmm("h3",hmm3,30);
+  // print_hmm("h1",hmm1,30);
+  // // Save to JSON
+  // save_hmm( "tmp_hmm.json", _expr );
+  // // Reload from JSON
+  // // first create another one
+  // create_hmm( "AAAF" );
+  // bica::sampler::HMM hmm2(_t,_o);
+  // std::cout << "__" << _expr << "__ with " << _nb_states << " states" << std::endl;  
+  // print_hmm("h2",hmm2,30);
+  // // then load
+  // load_hmm( "tmp_hmm.json" );
+  // bica::sampler::HMM hmm3(_t,_o);
+  // std::cout << "__" << _expr << "__ with " << _nb_states << " states" << std::endl;  
+  // print_hmm("h3",hmm3,30);
+
+  // Create and save trajectory
+  create_traj( 100 );
+  save_traj( "tmp_traj.json", _learn_data, _expr );
+  // Read Trajectory
+  _learn_data.clear();
+  load_traj( "tmp_traj.json" );
+  // for (auto it = _learn_data.begin(); it != _learn_data.begin()+10; ++it) {
+  //   std::cout << "s=" << (*it).id_s << "\to=" << (*it).id_o << std::endl;    
+  // }
   
-  
+
   return 0;
 }
 // ***************************************************************************
