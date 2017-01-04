832c7af3158fb918dbdfa9e3197b68d4d651c8a3
Alain <snowgoon88@gmail.com>
2017-01-04 09:20:35 +0100

    ADD normal and ring distributions

--- src/distribution.hpp
+++ src/distribution.hpp
@@ -46,6 +46,62 @@ public:
 
     return _data;
   }
+  // **************************************************** Distribution::normal
+  const Data& normal( unsigned int nb_data=100,
+					  double center_x = 0.0, double center_y = 0.0,
+					  double sigma_x = 1.0, double sigma_y = 1.0,
+					  double min_x = -1.0, double max_x = 1.0,
+					  double min_y = -1.0, double max_y = 1.0 )
+  {
+	 // generate seed
+    std::random_device random_seeder;
+    std::default_random_engine rnd_engine( random_seeder() );
+    // and random distribution
+    std::normal_distribution<double> rnd_x( center_x, sigma_x );
+    std::normal_distribution<double> rnd_y( center_y, sigma_y );
+
+	// Generate samples
+	_data.clear();
+    for( unsigned int i = 0; i < nb_data; ++i) {
+      Sample sample { rnd_x(rnd_engine), rnd_y(rnd_engine) };
+	  if( sample[0] < min_x ) sample[0] = min_x;
+	  if( sample[0] > max_x ) sample[0] = max_x;
+	  if( sample[1] < min_y ) sample[1] = min_y;
+	  if( sample[1] > max_y ) sample[1] = max_y;
+      _data.push_back( sample );
+    }
+
+    return _data;
+  }
+  // ******************************************************** Disibution::ring
+  /** ring from [0,1] uniforms distributions */
+  const Data& ring( unsigned int nb_data=100,
+					double center_x = 0.0, double center_y = 0.0,
+					double r_min = 0.0, double r_max = 0.5 )
+  {
+		 // generate seed
+    std::random_device random_seeder;
+    std::default_random_engine rnd_engine( random_seeder() );
+    // and random distribution
+    std::uniform_real_distribution<double> rnd_x( 0.0, 1.0 );
+    std::uniform_real_distribution<double> rnd_y( 0.0, 1.0 );
+
+	// Generate samples
+	_data.clear();
+    for( unsigned int i = 0; i < nb_data; ++i) {
+	  Sample sample { rnd_x(rnd_engine), rnd_y(rnd_engine) };
+	  double dist = sqrt( sample[0]*sample[0] + sample[1] * sample[1] );
+	  while( (dist < r_min) or (dist > r_max) ) {
+		sample = { rnd_x(rnd_engine), rnd_y(rnd_engine) };
+		dist = sqrt( sample[0]*sample[0] + sample[1] * sample[1] );
+	  }
+	  sample[0] += center_x;
+	  sample[1] += center_y;
+      _data.push_back( sample );
+    }
+
+    return _data;
+  }
   // ******************************************************* Distribution::R/W
   const Data& read(std::istream& is)
   {
--- test/test-018-distribution.cpp
+++ test/test-018-distribution.cpp
@@ -30,11 +30,45 @@ void test_uniform()
   ifile.close();
   dist_read.write( std::cout );
 }
+// **************************************************************** test_normal
+void test_normal()
+{
+  std::string filename = "normal.data";
+  
+  Distribution dist;
+  // generate
+  auto data = dist.normal( 30, 0.5, 0.5, 0.5, 0.1);
+  std::cout << "__CREATE" << std::endl;
+  dist.write( std::cout );
+
+  std::cout << "__WRITE and READ" << std::endl;
+  std::ofstream ofile( filename );
+  dist.write( ofile );
+  ofile.close();
+}
+// **************************************************************** test_normal
+void test_ring()
+{
+  std::string filename = "ring.data";
+  
+  Distribution dist;
+  // generate
+  auto data = dist.ring( 30, 0.5, 0.5, 0.5, 0.8);
+  std::cout << "__CREATE" << std::endl;
+  dist.write( std::cout );
+
+  std::cout << "__WRITE and READ" << std::endl;
+  std::ofstream ofile( filename );
+  dist.write( ofile );
+  ofile.close();
+}
 // ***************************************************************************
 // ********************************************************************** main
 int main(int argc, char *argv[])
 {
-  test_uniform();
+  // test_uniform();
+  // test_normal();
+  test_ring();
   return 0;
 }
 
